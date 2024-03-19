90c220933c7f2436821d7d50ef84b702ce6188d3
Martin Castaneda <martincastaneda75@yahoo.com>
2024-03-18 20:22:59 -0700

    updated frontend html/css

---
+++ project/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <footer class="container-fluid footer_section">
+            <p>
+                &copy; 2020 All Rights Reserved. Design by
+                <a href="https://html.design/">Free Html Templates</a>
+            </p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ project/src/components/Health.jsx
@@ -0,0 +1,28 @@
+function Health() {
+    return (
+        <section class="heathy_section layout_padding">
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-md-12 mx-auto">
+                        <div class="detail-box">
+                            <h2>
+                                HEALTHY MIND, HEALTHY BODY
+                            </h2>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                            </p>
+                            <div class="btn-box">
+                                <a href="">
+                                    READ MORE
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    )
+}
+export default Health;
