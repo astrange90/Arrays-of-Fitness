dbf85e963b59429b4a56f15867b2be20f839e080
Tim Van Deusen <145621518+timvandeusen@users.noreply.github.com>
2024-03-25 15:39:42 -0700

    Fixed all errors in the console while at the home page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,38 +1,38 @@
 function Header() {
     return (
-        <div class="hero_area">
-            <header class="header_section">
-                <div class="container-fluid">
-                    <nav class="navbar navbar-expand-lg custom_nav-container">
-                        <a class="navbar-brand" href="index.html">
+        <div className="hero_area">
+            <header className="header_section">
+                <div className="container-fluid">
+                    <nav className="navbar navbar-expand-lg custom_nav-container">
+                        <a className="navbar-brand" href="index.html">
                             <span>
                                 Arrays of Fitness
                             </span>
                         </a>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
                         </button>
 
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <div class="d-flex ml-auto flex-column flex-lg-row align-items-center">
-                                <ul class="navbar-nav  ">
-                                    <li class="nav-item active">
-                                        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <div className="d-flex ml-auto flex-column flex-lg-row align-items-center">
+                                <ul className="navbar-nav  ">
+                                    <li className="nav-item active">
+                                        <a className="nav-link" href="index.html">Home <span className="sr-only">(current)</span></a>
                                     </li>
-                                    <li class="nav-item ">
-                                        <a class="nav-link" href="why.html"> About </a>
+                                    <li className="nav-item ">
+                                        <a className="nav-link" href="why.html"> About </a>
                                     </li>
                                     <li />
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="trainer.html"> </a>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="trainer.html"> </a>
                                     </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="contact.html"> Login</a>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="contact.html"> Login</a>
                                     </li>
                                 </ul>
-                                <div class="user_option">
-                                    <form class="form-inline my-2 my-lg-0 ml-0 ml-lg-4 mb-3 mb-lg-0">
-                                        <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit"></button>
+                                <div className="user_option">
+                                    <form className="form-inline my-2 my-lg-0 ml-0 ml-lg-4 mb-3 mb-lg-0">
+                                        <button className="btn  my-2 my-sm-0 nav_search-btn" type="submit"></button>
                                     </form>
                                 </div>
 
@@ -44,13 +44,13 @@ function Header() {
             </header>
 
             {/* slider section  */}
-            <section class=" slider_section position-relative">
-      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <div class="container">
-              <div class="col-lg-10 col-md-11 mx-auto">
-                <div class="detail-box">
+            <section className=" slider_section position-relative">
+      <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <div className="container">
+              <div className="col-lg-10 col-md-11 mx-auto">
+                <div className="detail-box">
                   <div>
                     <h3>
                       Fitness
@@ -64,7 +64,7 @@ function Header() {
                     <p>
                       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse .
                     </p>
-                    <div class="">
+                    <div className="">
                       <a href="">
                         Contact Us
                       </a>
@@ -74,10 +74,10 @@ function Header() {
               </div>
             </div>
           </div>
-          <div class="carousel-item">
-            <div class="container">
-              <div class="col-lg-10 col-md-11 mx-auto">
-                <div class="detail-box">
+          <div className="carousel-item">
+            <div className="container">
+              <div className="col-lg-10 col-md-11 mx-auto">
+                <div className="detail-box">
                   <div>
                     <h3>
                       Fitness
@@ -91,7 +91,7 @@ function Header() {
                     <p>
                       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse .
                     </p>
-                    <div class="">
+                    <div className="">
                       <a href="">
                         Contact Us
                       </a>
@@ -101,10 +101,10 @@ function Header() {
               </div>
             </div>
           </div>
-          <div class="carousel-item">
-            <div class="container">
-              <div class="col-lg-10 col-md-11 mx-auto">
-                <div class="detail-box">
+          <div className="carousel-item">
+            <div className="container">
+              <div className="col-lg-10 col-md-11 mx-auto">
+                <div className="detail-box">
                   <div>
                     <h3>
                       Fitness
@@ -118,7 +118,7 @@ function Header() {
                     <p>
                       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse .
                     </p>
-                    <div class="">
+                    <div className="">
                       <a href="">
                         Contact Us
                       </a>
@@ -128,10 +128,10 @@ function Header() {
               </div>
             </div>
           </div>
-          <div class="carousel-item">
-            <div class="container">
-              <div class="col-lg-10 col-md-11 mx-auto">
-                <div class="detail-box">
+          <div className="carousel-item">
+            <div className="container">
+              <div className="col-lg-10 col-md-11 mx-auto">
+                <div className="detail-box">
                   <div>
                     <h3>
                       Fitness
@@ -145,7 +145,7 @@ function Header() {
                     <p>
                       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse .
                     </p>
-                    <div class="">
+                    <div className="">
                       <a href="">
                         Contact Us
                       </a>
@@ -155,10 +155,10 @@ function Header() {
               </div>
             </div>
           </div>
-          <div class="carousel-item">
-            <div class="container">
-              <div class="col-lg-10 col-md-11 mx-auto">
-                <div class="detail-box">
+          <div className="carousel-item">
+            <div className="container">
+              <div className="col-lg-10 col-md-11 mx-auto">
+                <div className="detail-box">
                   <div>
                     <h3>
                       Fitness
@@ -172,7 +172,7 @@ function Header() {
                     <p>
                       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse .
                     </p>
-                    <div class="">
+                    <div className="">
                       <a href="">
                         Contact Us
                       </a>
@@ -183,8 +183,8 @@ function Header() {
             </div>
           </div>
         </div>
-        <ol class="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <ol className="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
--- client/src/components/Trainer.jsx
+++ client/src/components/Trainer.jsx
@@ -1,78 +1,78 @@
 function Trainer() {
     return (
-        <section class="trainer_section layout_padding">
-            <div class="container">
-                <div class="heading_container">
+        <section className="trainer_section layout_padding">
+            <div className="container">
+                <div className="heading_container">
                     <h2>
                         Our Gym Trainers
                     </h2>
                 </div>
-                <div class="row">
-                    <div class="col-lg-4 col-md-6 mx-auto">
-                        <div class="box">
-                            <div class="name">
+                <div className="row">
+                    <div className="col-lg-4 col-md-6 mx-auto">
+                        <div className="box">
+                            <div className="name">
                                 <h5>
                                     Smirth Jon
                                 </h5>
                             </div>
-                            <div class="img-box">
-                                <img src="images/t1.jpg" alt=""/>
+                            <div className="img-box">
+                                <img src="./build/images/t1.jpg" alt=""/>
                             </div>
-                            <div class="social_box">
+                            <div className="social_box">
                                 <a href="">
-                                    <img src="images/facebook-logo.png" alt=""/>
+                                    <img src="./build/images/facebook-logo.png" alt=""/>
                                 </a>
                                 <a href="">
-                                    <img src="images/twitter.png" alt=""/>
+                                    <img src="./build/images/twitter.png" alt=""/>
                                 </a>
                                 <a href="">
-                                    <img src="images/instagram-logo.png" alt=""/>
+                                    <img src="./build/images/instagram-logo.png" alt=""/>
                                 </a>
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-4 col-md-6 mx-auto">
-                        <div class="box">
-                            <div class="name">
+                    <div className="col-lg-4 col-md-6 mx-auto">
+                        <div className="box">
+                            <div className="name">
                                 <h5>
                                     Jean Doe
                                 </h5>
                             </div>
-                            <div class="img-box">
-                                <img src="images/t2.jpg" alt=""/>
+                            <div className="img-box">
+                                <img src="./build/images/t2.jpg" alt=""/>
                             </div>
-                            <div class="social_box">
+                            <div className="social_box">
                                 <a href="">
-                                    <img src="images/facebook-logo.png" alt=""/>
+                                    <img src="./build/images/facebook-logo.png" alt=""/>
                                 </a>
                                 <a href="">
-                                    <img src="images/twitter.png" alt=""/>
+                                    <img src="./build/images/twitter.png" alt=""/>
                                 </a>
                                 <a href="">
-                                    <img src="images/instagram-logo.png" alt=""/>
+                                    <img src="./build/images/instagram-logo.png" alt=""/>
                                 </a>
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-4 col-md-6 mx-auto">
-                        <div class="box">
-                            <div class="name">
+                    <div className="col-lg-4 col-md-6 mx-auto">
+                        <div className="box">
+                            <div className="name">
                                 <h5>
                                     Alex Den
                                 </h5>
                             </div>
-                            <div class="img-box">
-                                <img src="images/t3.jpg" alt=""/>
+                            <div className="img-box">
+                                <img src="./build/images/t3.jpg" alt=""/>
                             </div>
-                            <div class="social_box">
+                            <div className="social_box">
                                 <a href="">
-                                    <img src="images/facebook-logo.png" alt=""/>
+                                    <img src="./build/images/facebook-logo.png" alt=""/>
                                 </a>
                                 <a href="">
-                                    <img src="images/twitter.png" alt=""/>
+                                    <img src="./build/images/twitter.png" alt=""/>
                                 </a>
                                 <a href="">
-                                    <img src="images/instagram-logo.png" alt=""/>
+                                    <img src="./build/images/instagram-logo.png" alt=""/>
                                 </a>
                             </div>
                         </div>
