278380a0cf556860559b9163fc0a0878c124653c
Isiarf <imrfauntleroy@yahoo.com>
2024-03-26 19:59:34 -0700

    Swapped ../routes/index.js files with the ../routes/api/index.js, as they were in the wrong order. Also added middleware to serve the data to the react side.

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
+const workoutRoutes = require('./workout-routes');
+const userRoutes = require('./user-routes');
 
-router.use('/api', apiRoutes);
+router.use('/users', userRoutes);
+router.use('/workouts', workoutRoutes);
 
 module.exports = router;
